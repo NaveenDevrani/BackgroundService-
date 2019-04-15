a2199a54204e25617bebdda925457528a80f2da2
naveen <naveendevrani06@gmail.com>
2019-04-15 12:44:07 +0530

    inital commit

---
+++ app/src/main/java/com/naveen/backgroundservice/MainActivity.java
@@ -0,0 +1,77 @@
+package com.naveen.backgroundservice;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.naveen.backgroundservice.services.BackgroundService;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_start_service, btn_stop_service;
+    Intent mServiceIntent;
+    private BackgroundService mYourService;
+    private String Tag="MainActivity()-->";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_stop_service=findViewById(R.id.btn_stop_service);
+        btn_start_service=findViewById(R.id.btn_start_service);
+
+        mYourService = new BackgroundService();
+        mServiceIntent = new Intent(this, mYourService.getClass());
+
+        btn_start_service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isMyServiceRunning(mYourService.getClass())) {
+                    startService(mServiceIntent);
+                }
+            }
+        });
+        btn_stop_service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(mServiceIntent);
+            }
+        });
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i ("Service status", "Running");
+                return true;
+            }
+        }
+        Log.i ("Service status", "Not running");
+        return false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (isMyServiceRunning(mYourService.getClass()))
+         stopService(mServiceIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(Tag,"onDestory");
+        if (!isMyServiceRunning(mYourService.getClass()))
+        startService(mServiceIntent);
+    }
+}
---
+++ app/src/main/java/com/naveen/backgroundservice/services/BackgroundService.java
@@ -0,0 +1,155 @@
+package com.naveen.backgroundservice.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.naveen.backgroundservice.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class BackgroundService  extends Service {
+
+    public int counter=0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O)
+            startMyOwnForeground();
+        else
+            startForeground(1, new Notification());
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void startMyOwnForeground()
+    {
+        String NOTIFICATION_CHANNEL_ID = "example.permanence";
+        String channelName = "Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setContentTitle("App is running in background")
+                 .setContentText(""+counter)
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        startTimer();
+        return START_STICKY;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stoptimertask();
+        Toast.makeText(this, "Background service ondestroy()", Toast.LENGTH_SHORT).show();
+//        Intent broadcastIntent = new Intent();
+//        broadcastIntent.setAction("restartservice");
+//        broadcastIntent.setClass(this, Restarter.class);
+//        this.sendBroadcast(broadcastIntent);
+    }
+
+
+
+    private Timer timer;
+    private TimerTask timerTask;
+    public void startTimer() {
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            public void run() {
+                Log.i("Count", "=========  "+ (counter++));
+                showSmallNotification("Background Service","my--"+counter);
+
+            }
+        };
+        timer.schedule(timerTask, 1000, 1000); //
+    }
+
+    public void stoptimertask() {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    private void showSmallNotification(String title, String message) {
+
+        NotificationCompat.Builder build;
+        Notification build1;
+        int notificationID = 234231;
+
+        NotificationManager mNotifyManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        build = new NotificationCompat.Builder(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+            inboxStyle.addLine(message);
+
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel mChannel = new NotificationChannel("ksjadf87", "service...", importance);
+            build1 = new Notification.Builder(this)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setChannelId("ksjadf87")
+                    .build();
+
+            assert mNotifyManager != null;
+            mNotifyManager.createNotificationChannel(mChannel);
+
+// Issue the notification.
+            mNotifyManager.notify(notificationID, build1);
+
+        } else {
+            NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+            inboxStyle.addLine(message);
+            build.setContentTitle(title)
+                    .setContentText(message)
+                    .setAutoCancel(true)
+                    .setStyle(inboxStyle)
+                    .setSmallIcon(R.mipmap.ic_launcher);
+
+            assert mNotifyManager != null;
+            mNotifyManager.notify(notificationID, build.build());
+//        }
+
+
+        }
+    }
+
+
+}
